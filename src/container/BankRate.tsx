2391df583022e8770f1af8197f18c5037d3b593e
Yagnik Vadi <vadiyagnik@gmail.com>
2023-02-15 17:30:30 +0530

    withdrow setup

--- src/container/BankRate.tsx
+++ src/container/BankRate.tsx
@@ -1,10 +1,28 @@
 import React, { FC, useState } from 'react'
 
 const BankRate: FC = () => {
-    const [bankBalance, setBankBalance] = useState<number | string>(5000);
+    const [bankBalance, setBankBalance] = useState<number>(5000);
+    const [getBalance, setGetBalance] = useState<number>()
+
+    // const withDrowBalance = (event: MouseEvent)=> {
+    //     event.preventDefault();
+    //     setGetBalance(event.target.value);
+    //     console.log(getBalance);
+    // }
+    const withDrowBalance = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        event.preventDefault();
+        
+    };
 
     return (
-        <h3>Current bank balance : {bankBalance}</h3>
+        <>
+            <h3>Current bank balance : {bankBalance}</h3>
+            <input type="number" value={getBalance} onChange={(event)=>setGetBalance}/>
+            <button onClick={()=>withDrowBalance}>withdraw</button>
+            <button>deposit</button>
+        </>
     )
 }
 
