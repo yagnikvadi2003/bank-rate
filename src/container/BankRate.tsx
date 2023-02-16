f16b659f80668d5940eabd2c87afa1a895430b84
Yagnik Vadi <vadiyagnik@gmail.com>
2023-02-16 22:03:59 +0530

    degine view setup

--- src/container/BankRate.tsx
+++ src/container/BankRate.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent, FC, MouseEvent, useState } from 'react';
 
 const BankRate: FC = (): JSX.Element => {
-    const [bankBalance, setBankBalance] = useState<number>(5000);
+    const [bankBalance, setBankBalance] = useState<number>(10000);
     const [getAmount, setGetAmount] = useState<number>(0);
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
@@ -20,16 +20,16 @@ const BankRate: FC = (): JSX.Element => {
 
     return (
         <div className="app">
-                <div className='stopwatch-card'>
-                    <label htmlFor="currentBalance">
-                        Current bank balance : {bankBalance}
-                    </label>
-                    <div className='buttons'>
-                        <input type="number" id="Amount" name="Enter Amount" value={getAmount} onChange={handleChange} />
-                        <button onClick={withDrawBalance}>withdraw</button>
-                        <button onClick={depositeBalance}>deposit</button>
-                    </div>
-                </div>
+            <div className='stopwatch-card'>
+                <label htmlFor="currentBalance">
+                    Current bank balance : {bankBalance}
+                </label>
+                <br /><br />
+                <input type="number" className="numberBox" id="Amount" name="Enter Amount" value={getAmount} onChange={handleChange} />
+                <br /><br />
+                <button onClick={withDrawBalance} style={{ margin: "0 5px" }} >withdraw</button>
+                <button onClick={depositeBalance} style={{ margin: "0 5px" }} >deposit</button>
+            </div>
         </div>
     )
 }
