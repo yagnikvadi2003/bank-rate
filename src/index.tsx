917e94552170cbee38c7b11b8f9db1da07cfedea
yagnikvadi2003 <vadiyagnik@gmail.com>
2023-02-21 09:13:34 +0530

    bug-fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from 'react-router-dom';
-import reportWebVitals from "./reportWebVitals";
 
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error('Failed to find the root element');
@@ -16,4 +16,7 @@ root.render(
   </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
